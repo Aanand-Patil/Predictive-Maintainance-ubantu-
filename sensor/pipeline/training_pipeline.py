4a03d538753ba3357e97814fbbbc26b74a5db751
Aanand-Patil <anandpatil6440@gmail.com>
2023-03-04 00:05:16 +0530

    basic structure created

---
+++ sensor/pipeline/training_pipeline.py
@@ -0,0 +1,10 @@
+from sensor.logger import logging
+from sensor.exception import SensorException
+import sys, os
+
+
+def start_traing_pipeline():
+    try:
+        pass
+    except Exception as e:
+        raise SensorException(e, sys)
