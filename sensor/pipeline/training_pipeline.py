4dd06d67a1d87ae3df05ac7f57762d4e8b0888d9
Aanand-Patil <anandpatil6440@gmail.com>
2023-03-04 00:46:11 +0530

    data_ingestion method added

--- sensor/pipeline/training_pipeline.py
+++ sensor/pipeline/training_pipeline.py
@@ -1,10 +1,21 @@
 from sensor.logger import logging
 from sensor.exception import SensorException
 import sys, os
+from sensor.entity import config_entity
+from sensor.components.data_ingestion import DataIngestion
 
 
-def start_traing_pipeline():
+def start_training_pipeline():
     try:
-        pass
+        training_pipeline_config = config_entity.TrainingPipelineConfig()
+
+        # data ingestion
+        data_ingestion_config = config_entity.DataIngestionConfig(
+            training_pipeline_config=training_pipeline_config
+        )
+        print(data_ingestion_config.to_dict())
+        data_ingestion = DataIngestion(data_ingestion_config=data_ingestion_config)
+        data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
+
     except Exception as e:
         raise SensorException(e, sys)
