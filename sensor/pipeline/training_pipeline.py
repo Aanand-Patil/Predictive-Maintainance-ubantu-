adda098f364c7128393c145a6da0067f51c855be
Aanand-Patil <anandpatil6440@gmail.com>
2023-03-04 13:14:39 +0530

    data_validation step added

--- sensor/pipeline/training_pipeline.py
+++ sensor/pipeline/training_pipeline.py
@@ -3,6 +3,7 @@ from sensor.exception import SensorException
 import sys, os
 from sensor.entity import config_entity
 from sensor.components.data_ingestion import DataIngestion
+from sensor.components.data_validation import DataValidation
 
 
 def start_training_pipeline():
@@ -17,5 +18,15 @@ def start_training_pipeline():
         data_ingestion = DataIngestion(data_ingestion_config=data_ingestion_config)
         data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
 
+        # data validation
+        data_validation_config = config_entity.DataValidationConfig(
+            training_pipeline_config=training_pipeline_config
+        )
+        data_validation = DataValidation(
+            data_validation_config=data_validation_config,
+            data_ingestion_artifact=data_ingestion_artifact,
+        )
+
+        data_validation_artifact = data_validation.initiate_data_validation()
     except Exception as e:
         raise SensorException(e, sys)
